5c5e6cc3bd50a30cce531683e969c736b0d290ea
Amir Hossein Parhizgar <am.parhizgar@gmail.com>
2025-12-02 00:08:11 +0330

    Implement day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,19 +7,14 @@ fun main() {
 
     val input = readInput("Day01")
     part1(input).println()
+    check(part2(testInput) == 6)
     part2(input).println()
 }
 
 fun part1(input: List<String>): Int {
     var result = 0
     input.fold(50) { currentValue, line ->
-        val sign = when (line.substring(0, 1)) {
-            "R" -> 1
-            "L" -> -1
-            else -> error("invalid character in $line")
-        }
-        val value = line.substring(1).toInt()
-        floorMod(currentValue + sign * value, 100).also {
+        floorMod(currentValue + line.sign() * line.value(), 100).also {
             if (it == 0) {
                 result++
             }
@@ -28,7 +23,25 @@ fun part1(input: List<String>): Int {
     return result
 }
 
-
 fun part2(input: List<String>): Int {
-    return input.size
+    var result = 0
+    input.fold(50) { currentValue, line ->
+        val inc = line.sign() * line.value()
+        floorMod(currentValue + inc, 100).also {
+            result += if (line.sign() > 0) {
+                (currentValue + inc) / 100
+            } else {
+                ((100 - currentValue) % 100 - inc) / 100
+            }
+        }
+    }
+    return result
+}
+
+private fun String.value(): Int = this.substring(1).toInt()
+
+private fun String.sign(): Int = when (this.first()) {
+    'R' -> 1
+    'L' -> -1
+    else -> error("invalid character ${this.first()}")
 }
